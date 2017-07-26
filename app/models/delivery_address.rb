34ebf683f306fa1da2af5d9aa778dfb3f4d6cfc5
FAVIO VELEZ <favio.velez@hotmail.com>
2017-07-26 14:29:45 -0500

    validations to store, prospect, delivery address and billing address and user

--- app/models/delivery_address.rb
+++ app/models/delivery_address.rb
@@ -4,4 +4,22 @@ class DeliveryAddress < ActiveRecord::Base
   has_many :stores
   has_many :prospects
   has_one :carrier
+
+  validates :zipcode, length: { is: 5, message: 'El código postal debe contener 5 caracteres.'}
+
+  validates :zipcode, presence: { message: 'Por favor anote el código postal.'}
+
+  validates :street, presence: { message: 'Debe escribir la calle.'}
+
+  validates :exterior_number, presence: { message: 'Debe escribir el número exterior.'}
+
+  validates :neighborhood, presence: { message: 'Debe escribir la colonia.'}
+
+  validates :city, presence: { message: 'Debe escribir la ciudad.'}
+
+  validates :state, presence: { message: 'Debe escribir el estado.'}
+
+  validates :country, presence: { message: 'Debe escribir el país.'}
+
+
 end
