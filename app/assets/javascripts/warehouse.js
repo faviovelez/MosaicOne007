85bd78c824a7c5d02bac3d23d51face6d9fe9a5d
Oscar Hugo Cardenas Lopez <oscardev@cacyberlo.com.mx>
2017-08-05 16:24:02 -0500

    add a new product in list witout erros

--- app/assets/javascripts/warehouse.js
+++ app/assets/javascripts/warehouse.js
@@ -5,85 +5,73 @@ $(function(){
     variable: 'rc'
   };
 
-  $('#movement_product_id').select2({
-    maximumSelectionLength: 1,
-    multiple: true
-  })
-  .change(function(){
-    if ($(this).val() === null) {
-      $('#product_1').remove();
+  var formatState = function(state){
+    var r = /\d+/;
+    var code = state.text.match(r);
+    return code;
+  };
+
+  var changeAction = function(element, inc, dec){
+    if ($(element).val() === null) {
+      $('td[id$=product'+dec+']').remove();
     } else {
       $.ajax({
-        url: '/warehouse/get/' + $(this).val(),
+        url: '/warehouse/get/' + $(element).val(),
         method: 'get'
       }).done(function(response) {
         var template = _.template(
-          $( "script.template" ).html()
+          $("script.template").html()
         );
         var image = response.images.length > 0 ?
           response.images[0].image.small.url :
           $('#not_image_temp').attr('src');
         var link = $('#link_to_images').attr('href').replace(/\d+/g, response.product.id);
-        var id = "product_" + $('select').length;
-        var input = '<a href="#" id="newProduct" class="btn btn-default productInfoData">Agregar nuevo</a>';
-        $('#new_movement').append(
+        var id = "product" + dec;
+        $('#trForProduct' + dec).append(
           template( {
-            unique_code: response.product.unique_code,
             description: response.product.description,
             image:       image,
             link:        link,
             id:          id
           } )
         );
-        if ($('#newProduct').length === 0){
-          $('#new_movement').append(input);
-        }
-        $('#newProduct').click(function(){
-          var inc = ($('select').length + 1);
-          var id = 'select_product_' + inc;
-          var label = '<label for="' + id + '">Selecionar producto</label><br />';
-          var select = '<select id="' + id  + '"></select><br />';
-          $('#new_movement').append(label).append(select);
-          $('#' + id)
-          .append($('#movement_product_id').html())
-          .select2({
-            maximumSelectionLength: 1,
-            multiple: true
-          })
-          .change(function(){
-            var inc = ($('select').length);
-            if ($(this).val() === null) {
-              $('#product_' + inc).remove();
-            } else {
-              $.ajax({
-                url: '/warehouse/get/' + $(this).val(),
-                method: 'get'
-              }).done(function(response) {
-                var template = _.template(
-                  $( "script.template" ).html()
-                );
-                var image = response.images.length > 0 ?
-                  response.images[0].image.small.url :
-                  $('#not_image_temp').attr('src');
-                var link = $('#link_to_images').attr('href').replace(/\d+/g, response.product.id);
-                var id = "product_" + $('select').length;
-                $('#new_movement').append(
-                  template( {
-                    unique_code: response.product.unique_code,
-                    description: response.product.description,
-                    image:       image,
-                    link:        link,
-                    id:          id
-                  } )
-                );
-              });
-            }
+        $('#numProduct_'+ id).mask("000", {placeholder: "___"}).css({'text-align': 'center'});
+        if ($('tr[id^=trForProduct]').length === dec){
+          $('#addNew' + id).click(function(){
+            var tr = "<tr id='trForProduct"+ inc +"'>" +
+              "<td class='select'>" +
+              "<select id='selectForProduct"+ inc +"'></select>" +
+              "<a href='#' class='btn btn-danger hidden'>X</a>" +
+              "</td>" +
+              "</tr>";
+            $('tbody').append(tr);
+            $('#selectForProduct'+ inc).append($('#product1').html());
+            $('#selectForProduct'+ inc).select2({
+              templateSelection: formatState,
+              multiple: true,
+              maximumSelectionLength: 1
+            })
+            .change(function(){
+              var dec = parseInt($(this).attr('id').match(/\d+/)[0]);
+              changeAction(this, dec + 1, dec);
+            });
+            $(this).addClass('hidden');
+            return false;
           });
-          $('#newProduct').appendTo('#new_movement');
-          return false;
-        });
+        } else {
+          $('#addNew' + id).addClass('hidden');
+        }
       });
     }
+  };
+
+  $('#product1').select2({
+    templateSelection: formatState,
+    multiple: true,
+    maximumSelectionLength: 1
+  })
+  .change(function(){
+    changeAction(this, 2, 1);
   });
 
 });
