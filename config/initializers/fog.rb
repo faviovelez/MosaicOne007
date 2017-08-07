0b463b10da431065147a6924ac9b81ec553df3db
FAVIO VELEZ <favio.velez@hotmail.com>
2017-08-07 12:57:02 -0500

    some media queries and changes to fog.rb for correct conection

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -8,8 +8,8 @@ elsif Rails.env.production?
     config.fog_credentials = {
       :provider               => 'AWS',                         # required
       :aws_access_key_id      => ENV['aws_access_key_id'],      # required
-      :aws_secret_access_key  => ENV['aws_secret_access_key']   # required
-      #:region                 => 'us-east-1'                   # optional, defaults to 'us-east-1'
+      :aws_secret_access_key  => ENV['aws_secret_access_key'],   # required
+      :region                 => 'us-west-2'                   # optional, defaults to 'us-east-1'
       #:host                   => 's3.example.com',             # optional, defaults to nil
       #:endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
     }
@@ -24,8 +24,8 @@ elsif Rails.env.development?
     config.fog_credentials = {
       :provider               => 'AWS',                         # required
       :aws_access_key_id      => ENV['aws_access_key_id'],      # required
-      :aws_secret_access_key  => ENV['aws_secret_access_key']   # required
-      #:region                 => 'us-east-1'                   # optional, defaults to 'us-east-1'
+      :aws_secret_access_key  => ENV['aws_secret_access_key'],   # required
+      :region                 => 'us-west-2'                   # optional, defaults to 'us-east-1'
       #:host                   => 's3.example.com',             # optional, defaults to nil
       #:endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
     }
