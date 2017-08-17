0de7c4b55f5f36590895290005d84ab8f091e347
FAVIO VELEZ <favio.velez@hotmail.com>
2017-08-17 12:17:27 -0500

    added suppliers and configuration, modified navs

--- db/migrate/20170817151619_remove_business_units_supplier_table.rb
+++ db/migrate/20170817151619_remove_business_units_supplier_table.rb
@@ -1,7 +1,11 @@
 class RemoveBusinessUnitsSupplierTable < ActiveRecord::Migration
   def change
+<<<<<<< 195b2bdd9a4f72323a76cea1a9f8f11b4c244aaa
     if table exists? :business_units_suppliers
       drop_table :business_units_suppliers
     end
+=======
+    drop_table :business_units_suppliers
+>>>>>>> added suppliers and configuration, modified navs
   end
 end
