edfb5eccf676d299f287bb80094fe946b91f358b
FAVIO VELEZ <favio.velez@hotmail.com>
2017-10-19 20:22:00 -0500

    changes needed to production

--- app/helpers/inventories_helper.rb
+++ app/helpers/inventories_helper.rb
@@ -1,9 +1,9 @@
 module InventoriesHelper
 
-  def inventory_alert(product)
-    if (product.inventory.alert == true && product.inventory.alert_type == 'bajo')
+  def inventory_alert_store(inventory)
+    if (inventory.alert == true && inventory.alert_type == 'bajo')
       @label = content_tag(:span, 'bajo', class: 'label label-warning')
-    elsif (product.inventory.alert == true && product.inventory.alert_type == 'crítico')
+    elsif (inventory.alert == true && inventory.alert_type == 'crítico')
       @label = content_tag(:span, 'crítico', class: 'label label-danger')
     else
       @label = content_tag(:span, 'adecuado', class: 'label label-success')
@@ -11,8 +11,23 @@ module InventoriesHelper
     @label
   end
 
-  def packages(product)
-    @packages = product.inventory.quantity / product.pieces_per_package
+  def inventory_alert_corp(inventory)
+    if (inventory.alert == true && inventory.alert_type == 'bajo')
+      @label = content_tag(:span, 'bajo', class: 'label label-warning')
+    elsif (inventory.alert == true && inventory.alert_type == 'crítico')
+      @label = content_tag(:span, 'crítico', class: 'label label-danger')
+    else
+      @label = content_tag(:span, 'adecuado', class: 'label label-success')
+    end
+    @label
+  end
+
+  def quantity(inventory)
+    @inventory = inventory.quantity
+  end
+
+  def packages(inventory, product)
+    @packages = inventory.quantity / product.pieces_per_package
   end
 
 end
