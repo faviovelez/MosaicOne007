77848a32cbd3cfcc3d2095399b30b8d7a03c4c80
FAVIO VELEZ <favio.velez@hotmail.com>
2017-11-20 17:18:38 -0600

    new branch for production

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -3,14 +3,14 @@ if Rails.env.test?
     config.storage = :file
     config.enable_processing = false
   end
-
+ 
 elsif Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
       :provider               => 'AWS',                         # required
       :aws_access_key_id      => ENV['aws_access_key_id'],      # required
-      :aws_secret_access_key  => ENV['aws_secret_access_key'],   # required
-      :region                 => 'us-west-1'                   # optional, defaults to 'us-east-1'
+      :aws_secret_access_key  => ENV['aws_secret_access_key'],  # required
+      :region                 => ENV['dev_region']                    # optional, defaults to 'us-east-1'
       #:host                   => 's3.example.com',             # optional, defaults to nil
       #:endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
     }
@@ -26,8 +26,8 @@ elsif Rails.env.development?
     config.fog_credentials = {
       :provider               => 'AWS',                         # required
       :aws_access_key_id      => ENV['aws_access_key_id'],      # required
-      :aws_secret_access_key  => ENV['aws_secret_access_key'],   # required
-      :region                 => 'us-east-1'                 # optional, defaults to 'us-east-1'
+      :aws_secret_access_key  => ENV['aws_secret_access_key'],  # required
+      :region                 => ENV['prod_region']                    # optional, defaults to 'us-east-1'
       #:host                   => 's3.example.com',             # optional, defaults to nil
       #:endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
     }
