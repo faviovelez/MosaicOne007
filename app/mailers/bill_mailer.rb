bad55036597ebb6b0c918475f3ab52a4bd63d8ac
Favio Velez <favio.velez@hotmail.com>
2018-01-12 14:17:39 -0600

    fix in mailer

--- app/mailers/bill_mailer.rb
+++ app/mailers/bill_mailer.rb
@@ -3,21 +3,21 @@ class BillMailer < ApplicationMailer
   default from: "facturas@plataforma-dc.com"
   layout 'mailer'
 
-  def send_bill_files(bill)
-    @mails = []
-    mail = bill.receiving_company.prospects.first.email
-    mail_2 = bill.receiving_company.prospects.first.email_2
-    mail_3 = bill.receiving_company.prospects.first.email_3
-    @mails << mail unless (mail == nil || mail == '')
-    @mails << mail_2 unless (mail_2 == nil || mail_2 == '')
-    @mails << mail_3 unless (mail_3 == nil || mail_3 == '')
-    attachments['Factura.pdf'] = File.read(bill.pdf_url)
-    attachments['Factura.xml'] = File.read(bill.xml_url)
-      mail(
-        to:
-        bcc: @mails,
-        subject: "Facturas Diseños de Cartón"
-      )
-  end
+#  def send_bill_files(bill)
+#    @mails = []
+#    mail = bill.receiving_company.prospects.first.email
+#    mail_2 = bill.receiving_company.prospects.first.email_2
+#    mail_3 = bill.receiving_company.prospects.first.email_3
+#    @mails << mail unless (mail == nil || mail == '')
+#    @mails << mail_2 unless (mail_2 == nil || mail_2 == '')
+#    @mails << mail_3 unless (mail_3 == nil || mail_3 == '')
+#    attachments['Factura.pdf'] = File.read(bill.pdf_url)
+#    attachments['Factura.xml'] = File.read(bill.xml_url)
+#      mail(
+#        to:
+#        bcc: @mails,
+#        subject: "Facturas Diseños de Cartón"
+#      )
+#  end
 
 end
