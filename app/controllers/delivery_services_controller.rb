eb851476f8b39c72d8cd1c7e4c5dbbf77cdfe4c5
Favio Velez <favio.velez@hotmail.com>
2018-05-03 15:51:47 -0500

    first part of delivery services

--- app/controllers/delivery_services_controller.rb
+++ app/controllers/delivery_services_controller.rb
@@ -3,7 +3,7 @@ class DeliveryServicesController < ApplicationController
   def report_type
     if params[:options] == 'Seleccionar día'
       date = Date.parse(params[:date]) unless (params[:date] == nil || params[:date] == '')
-      intial_date = date.midnight + 6.hours
+      initial_date = date.midnight + 6.hours
       final_date = date.end_of_day + 6.hours
     elsif params[:options] == 'Mes actual'
       intial_date = Date.today.beginning_of_month.midnight + 6.hours
@@ -26,10 +26,10 @@ class DeliveryServicesController < ApplicationController
       end
     elsif params[:report_type] == 'Base de datos'
       if params[:companies].include?('Todas')
-        @delivery_services = DeliveryService.joins(service_offered: :ticket).where(store_id: current_user.store.id, created_at: initial_date..final_date, tickets: {ticket_type: ['venta', 'devolución']}).where.not(service_offereds: {ticket_id: nil}).order(:id)
+        @delivery_services = DeliveryService.includes(service_offered: :ticket).where(store_id: current_user.store.id, created_at: initial_date..final_date, tickets: {ticket_type: ['venta', 'devolución']}).where.not(service_offereds: {ticket_id: nil}).order(:id)
       else
         companies = params[:companies]
-        @delivery_services = DeliveryService.joins(service_offered: [:ticket, :service]).where(store_id: current_user.store.id, created_at: initial_date..final_date, services: {delivery_company: companies}, tickets: {ticket_type: ['venta', 'devolución']}).where.not(service_offereds: {ticket_id: nil}).order(:id)
+        @delivery_services = DeliveryService.includes(service_offered: [:ticket, :service]).where(store_id: current_user.store.id, created_at: initial_date..final_date, services: {delivery_company: companies}, tickets: {ticket_type: ['venta', 'devolución']}).where.not(service_offereds: {ticket_id: nil}).order(:id)
       end
       if @delivery_services == []
         redirect_to root_path, alert: 'La fecha seleccionada no tiene registros, por favor elija otra'
@@ -46,6 +46,12 @@ class DeliveryServicesController < ApplicationController
   end
 
   def delivery_services_report
+  end
+
+  def cost_save_view
+  end
+
+  def save_cost
   end
 
 end
