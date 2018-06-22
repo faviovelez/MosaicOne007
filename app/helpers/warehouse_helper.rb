57973c0c5d77ee40f3a53ca423f65156707557b3
Favio Velez <favio.velez@hotmail.com>
2018-06-22 13:33:40 -0500

    changes for Rosy

--- app/helpers/warehouse_helper.rb
+++ app/helpers/warehouse_helper.rb
@@ -35,34 +35,6 @@ module WarehouseHelper
       :image_url, :thumb) || 'product_thumb.png'
   end
 
-  def warehouse_name(order)
-    user = order.users.joins(:role).where('roles.name' => ['warehouse-staff', 'warehouse-admin']).first
-    if user == nil
-      name = content_tag(:span, 'sin asignar', class: 'label label-danger')
-    else
-      name = user.first_name.capitalize + " " + user.last_name.capitalize
-    end
-    name
-  end
-
-  def driver_name(order)
-    delivery = order.delivery_attempt
-    if delivery == nil
-      name = content_tag(:span, 'sin asignar', class: 'label label-danger')
-    else
-      name = delivery.driver.first_name.capitalize + " " + delivery.driver.last_name.capitalize
-    end
-    name
-  end
-
-  def driver_names
-    @drivers = []
-    Driver.all.select("CONCAT(first_name, ' ', last_name) AS name, id").each do |driver|
-      @drivers << [driver.name, driver.id]
-    end
-    @drivers
-  end
-
 # CONFIRMAR SI VOY A USAR ESTE MÉTODO (ES PARA USARLO EN LA PREPARACIÓN DE ALMACÉN PARA ACCEDER A AGREGAR PAQUETES AL DELIVERY)
   def find_user(order)
     @finded_user = nil
