6dc955ef1a68e65a407dde2a72c9044e191210a7
Favio Velez <favio.velez@hotmail.com>
2018-11-09 11:16:02 -0600

    delete debugger

--- app/controllers/warehouse_controller.rb
+++ app/controllers/warehouse_controller.rb
@@ -84,7 +84,6 @@ class WarehouseController < ApplicationController
 
   def ready_orders
     banned_prospects_validation
-    debugger
     @orders = Order.where(status: 'preparado', corporate: current_user.store).where.not(prospect_id: @banned_prospects)
   end
 
