3bbc870da1ebe068eb665c00ff35340783c95d47
Favio Velez <favio.velez@hotmail.com>
2019-09-19 21:16:50 -0500

    more fixes

--- app/models/product.rb
+++ app/models/product.rb
@@ -51,6 +51,47 @@ class Product < ActiveRecord::Base
 
   after_save :validate_line
 
+  after_save :validate_pieces
+
+  after_save :delete_stores_inventories
+
+  def delete_stores_inventories
+    if self.shared == false || current == false
+      Store.where.not(store_type: 2).each do |store|
+        store_id = store.id
+        products = Product.where("shared = true AND current = true AND (classification = 'de línea' OR classification = 'especial') AND child_id is NULL OR store_id = #{store_id}").order(:id).pluck(:id)
+        products.each do |product|
+          if StoresInventory.where(store: store, product_id: product).count < 1
+            StoresInventory.create(store: store, product_id: product)
+          end
+        end
+
+        inventories = StoresInventory.where(store: store).order(:id)
+        bad_inventories = []
+
+        inventories.each do |inv|
+          if !products.include?(inv.product_id)
+            bad_inventories << inv
+          end
+        end
+
+        bad_inventories.each do |inv|
+          inv.delete
+        end
+      end
+
+      StoresInventory.where(store_id: 1).find_each do |inv|
+        inv.delete
+      end
+    end
+  end
+
+  def validate_pieces
+    if self.pieces_per_package == nil || self.pieces_per_package < 1
+      self.update(pieces_per_package: 1)
+    end
+  end
+
   def validate_special
     if self.classification == 'especial' && self.line != 'diseños especiales'
       self.update(line: 'diseños especiales')
